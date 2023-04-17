9cc4d20b73a6537a9fa3ca34bc579fc7a604806e
Benjamin Filiatrault <benjamin.filiatrault@hotmail.com>
2023-04-17 06:00:40 -0400

    adds reset & operators

--- src/button_system.rs
+++ src/button_system.rs
@@ -14,7 +14,8 @@ pub fn button_system(
 
         match *interaction {
             Interaction::Clicked => {
-                token_state.value.push_str(text.sections[0].value.as_str());
+                // token_state.value.push_str(text.sections[0].value.as_str());
+                handle_interaction(&mut token_state, text.sections[0].value.as_str());
                 *color = PRESSED_BUTTON.into();
             }
             Interaction::Hovered => {
@@ -25,4 +26,23 @@ pub fn button_system(
             }
         }
     }
+}
+
+
+fn handle_interaction(mut token_state: &mut TokenManager, btn_token: &str) {
+    if btn_token == "AC" {
+        token_state.value = "0".to_string();
+        return;
+    }
+
+    // We don't want to push stuff
+    // after 0 if that's the only value
+    if token_state.value == "0" {
+        // TODO: handle exceptions like operators and decimal pointer
+        token_state.value = btn_token.to_string();
+        return;
+    }
+
+
+    token_state.value.push_str(btn_token);
 }
--- src/token_manager.rs
+++ src/token_manager.rs
@@ -3,22 +3,21 @@ use bevy::prelude::*;
 use crate::setup::TokenText;
 
 #[derive(Resource, Debug, Clone)]
-pub struct TokenManager { 
+pub struct TokenManager {
     pub(crate) value: String,
-    pub(crate) default: String,
 }
 
 impl FromWorld for TokenManager {
     fn from_world(_world: &mut World) -> Self {
         // You have full access to anything in the ECS from here.
         // For instance, you can mutate other resources:
-        TokenManager { value: String::from(""), default: String::from("0") }
+        TokenManager { value: String::from("0") } 
     }
 }
 
 pub fn token_system(mut query: Query<&mut Text, With<TokenText>>, token_state: ResMut<TokenManager>,) {
     for mut text in &mut query {
         // Update the value of the second section
-        text.sections[0].value = token_state.value.to_owned();
+            text.sections[0].value = token_state.value.to_owned();
     }
 }
